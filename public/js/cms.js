a24598303e2708e7f3e0cdc4db7f647f407641fc
David Perez <david.perez@us.abb.com>
2018-05-21 23:06:24 -0400

    burger jquery logic

--- public/js/cms.js
+++ public/js/cms.js
@@ -1,54 +1,53 @@
 $(document).ready(function() {
-  // Getting jQuery references to the post body, title, form, and author select
   var bodyInput = $("#body");
-  var titleInput = $("#title");
+  var burgerInput = $("#burger");
   var cmsForm = $("#cms");
-  var authorSelect = $("#author");
+  var customerSelect = $("#customer");
   // Adding an event listener for when the form is submitted
   $(cmsForm).on("submit", handleFormSubmit);
   // Gets the part of the url that comes after the "?" (which we have if we're updating a post)
   var url = window.location.search;
-  var postId;
-  var authorId;
-  // Sets a flag for whether or not we're updating a post to be false initially
+  var burgerId;
+  var customerId;
+  // Sets a flag for whether or not we're updating a burger to be false initially
   var updating = false;
 
-  // If we have this section in our url, we pull out the post id from the url
-  // In '?post_id=1', postId is 1
-  if (url.indexOf("?post_id=") !== -1) {
-    postId = url.split("=")[1];
-    getPostData(postId, "post");
+  // If we have this section in our url, we pull out the burger id from the url
+  // In '?post_id=1', burgerId is 1
+  if (url.indexOf("?burger_id=") !== -1) {
+    burgerId = url.split("=")[1];
+    getPostData(burgerId, "burger");
   }
-  // Otherwise if we have an author_id in our url, preset the author select box to be our Author
-  else if (url.indexOf("?author_id=") !== -1) {
-    authorId = url.split("=")[1];
+  // Otherwise if we have an customer_id in our url, preset the customer select box to be our Customer
+  else if (url.indexOf("?customer_id=") !== -1) {
+    customerId = url.split("=")[1];
   }
 
-  // Getting the authors, and their posts
-  getAuthors();
+  // Getting the customers, and their burgers
+  getCustomers();
 
   // A function for handling what happens when the form to create a new post is submitted
   function handleFormSubmit(event) {
     event.preventDefault();
     // Wont submit the post if we are missing a body, title, or author
-    if (!titleInput.val().trim() || !bodyInput.val().trim() || !authorSelect.val()) {
+    if (!burgerInput.val().trim() || !bodyInput.val().trim() || !customerSelect.val()) {
       return;
     }
     // Constructing a newPost object to hand to the database
-    var newPost = {
-      title: titleInput
+    var newBurger = {
+      burger_name: burgerInput
         .val()
         .trim(),
       body: bodyInput
         .val()
         .trim(),
-      AuthorId: authorSelect.val()
+      customerId: customerSelect.val()
     };
 
     // If we're updating a post run updatePost to update a post
     // Otherwise run submitPost to create a whole new post
     if (updating) {
-      newPost.id = postId;
+      newPost.id = burgerId;
       updatePost(newPost);
     }
     else {
@@ -63,14 +62,14 @@ $(document).ready(function() {
     });
   }
 
-  // Gets post data for the current post if we're editing, or if we're adding to an author's existing posts
+  // Gets burger data for the current burger if we're editing, or if we're adding to an customer's existing burgers
   function getPostData(id, type) {
     var queryUrl;
     switch (type) {
-    case "post":
+    case "burger":
       queryUrl = "/api/burgers/" + id;
       break;
-    case "author":
+    case "customer":
       queryUrl = "/api/customers/" + id;
       break;
     default:
@@ -78,54 +77,54 @@ $(document).ready(function() {
     }
     $.get(queryUrl, function(data) {
       if (data) {
-        console.log(data.AuthorId || data.id);
+        console.log(data.customerId || data.id);
         // If this post exists, prefill our cms forms with its data
-        titleInput.val(data.title);
+        burgerInput.val(data.title);
         bodyInput.val(data.body);
-        authorId = data.AuthorId || data.id;
-        // If we have a post with this id, set a flag for us to know to update the post
+        customerId = data.customerId || data.id;
+        // If we have a burger with this id, set a flag for us to know to update the burger
         // when we hit submit
         updating = true;
       }
     });
   }
 
-  // A function to get Authors and then render our list of Authors
-  function getAuthors() {
-    $.get("/api/customers", renderAuthorList);
+  // A function to get Customers and then render our list of Customers
+  function getCustomers() {
+    $.get("/api/customers", renderCustomerList);
   }
-  // Function to either render a list of authors, or if there are none, direct the user to the page
-  // to create an author first
-  function renderAuthorList(data) {
+  // Function to either render a list of customers, or if there are none, direct the user to the page
+  // to create an customer first
+  function renderCustomerList(data) {
     if (!data.length) {
       window.location.href = "/cms";
     }
     $(".hidden").removeClass("hidden");
     var rowsToAdd = [];
     for (var i = 0; i < data.length; i++) {
-      rowsToAdd.push(createAuthorRow(data[i]));
+      rowsToAdd.push(createCustomerRow(data[i]));
     }
-    authorSelect.empty();
+    customerSelect.empty();
     console.log(rowsToAdd);
-    console.log(authorSelect);
-    authorSelect.append(rowsToAdd);
-    authorSelect.val(authorId);
+    console.log(customerSelect);
+    customerSelect.append(rowsToAdd);
+    customerSelect.val(customerId);
   }
 
-  // Creates the author options in the dropdown
-  function createAuthorRow(author) {
+  // Creates the customer options in the dropdown
+  function createCustomerRow(customer) {
     var listOption = $("<option>");
-    listOption.attr("value", author.id);
-    listOption.text(author.name);
+    listOption.attr("value", customer.id);
+    listOption.text(customer.name);
     return listOption;
   }
 
-  // Update a given post, bring user to the blog page when done
-  function updatePost(post) {
+  // Update a given burger, bring user to the burger page when done
+  function updatePost(burger) {
     $.ajax({
       method: "PUT",
       url: "/api/burgers",
-      data: post
+      data: burger
     })
       .then(function() {
         window.location.href = "/burgers";
